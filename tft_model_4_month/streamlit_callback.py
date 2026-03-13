d50d4f8f54316a4aa17aa7a70d4bcb5fdb2a3340
srushti sodavadiya <sodavadiyasodavadiya@gmail.com>
2026-03-13 16:56:03 +0530

    Add files via upload

---
+++ tft_model_4_month/streamlit_callback.py
@@ -0,0 +1,10 @@
+import pytorch_lightning as pl
+
+class ModelTrainingCallback(pl.callbacks.Callback):
+    pass
+
+class StreamlitCallback(pl.callbacks.Callback):
+    pass
+
+class StreamlitProgressCallback(pl.callbacks.Callback):
+    pass
